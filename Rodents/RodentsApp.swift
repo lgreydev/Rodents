10f0034bad742dbba91e5d6c620504c4bc74d971
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-07-21 21:57:34 -0400

    Initial Commit

---
+++ Rodents/RodentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RodentsApp.swift
+//  Rodents
+//
+//  Created by Sergey Lukaschuk on 2023-07-21.
+//
+
+import SwiftUI
+
+@main
+struct RodentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
